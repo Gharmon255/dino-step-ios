b26af88e040e7598d32424d1355a2c6c3281eb70
Gary Harmon <gharmon255@gmail.com>
2026-05-29 18:56:41 -0400

    Add watchOS companion target

---
+++ Dino Step Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dino Step Watch Watch App
+//
+//  Created by Gary  on 5/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dino Step Watch Watch App/Dino_Step_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dino_Step_WatchApp.swift
+//  Dino Step Watch Watch App
+//
+//  Created by Gary  on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Dino_Step_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
