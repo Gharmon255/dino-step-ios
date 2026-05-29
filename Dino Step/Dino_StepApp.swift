d15dcb2aff42893446e5ea69a06a10eab8b3d0a4
Gary Harmon <gharmon255@gmail.com>
2026-05-29 18:02:45 -0400

    Create Xcode iOS starter project

---
+++ Dino Step/Dino_StepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dino_StepApp.swift
+//  Dino Step
+//
+//  Created by Gary  on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Dino_StepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
